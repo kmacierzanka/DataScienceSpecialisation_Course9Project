caeba9b46e078f508fce22f6bb07903390d65404
kmacierzanka <krzysztof.macierzanka@gmail.com>
2020-06-27 14:54:24 +1200

    Finished app

--- CLT_Visualisation/server.R
+++ CLT_Visualisation/server.R
@@ -1,1 +1,55 @@
 library(shiny)
+
+shinyServer(function(input, output) {
+    output$plot <- renderPlot({
+        library(ggplot2)
+        
+        if (input$check_binom) {
+            binom_means <- as.data.frame(matrix(nrow = input$slider_binom_sam, ncol = 2))
+            set.seed(7345)
+            for (i in 1:input$slider_binom_sam) {
+                binom_means[i, 2] <- mean(rbinom(input$slider_binom_obs, input$slider_binom_obs, 0.5))
+            }
+            binom <- geom_histogram(data = binom_means, mapping = aes(x = scale(V2), y = ..density..,
+                                                                      fill = "blue", colour = "darkblue"),
+                                    binwidth = 0.25, alpha = 0.1, lwd = 0.5)
+        }
+        
+        if (input$check_pois) {
+            pois_means <- as.data.frame(matrix(nrow = input$slider_pois_sam, ncol = 2))
+            set.seed(2634)
+            for (i in 1:input$slider_pois_sam) {
+                pois_means[i, 2] <- mean(rpois(input$slider_pois_obs, 5))
+            }
+            pois <- geom_histogram(data = pois_means, mapping = aes(x = scale(V2), y = ..density..,
+                                                                    fill = "yellow", colour = "orange"),
+                                    binwidth = 0.25, alpha = 0.1, lwd = 0.5)
+        }
+        
+        if (input$check_exp) {
+            exp_means <- as.data.frame(matrix(nrow = input$slider_exp_sam, ncol = 2))
+            set.seed(2634)
+            for (i in 1:input$slider_exp_sam) {
+                exp_means[i, 2] <- mean(rexp(input$slider_exp_obs, 5))
+            }
+            expon <- geom_histogram(data = exp_means, mapping = aes(x = scale(V2), y = ..density..,
+                                                                    fill = "green", colour = "darkgreen"),
+                                   binwidth = 0.25, alpha = 0.1, lwd = 0.5)
+        }
+        
+        ggplot(data = data.frame(x = c(-5, 5)), aes(x = x)) +
+            theme_minimal() + labs(x = "x", y = "Proportion") +
+            stat_function(fun = dnorm, geom = "line",
+                          args = list(mean = 0, sd = 1),
+                          lwd = 2, colour = "tomato") +
+            scale_fill_manual(name = "Sampled from:",
+                              values = c("blue", "yellow", "green"),
+                              labels = c("Binomial", "Poisson", "Exponential")) +
+            scale_colour_manual(name = "Sampled from:",
+                              values = c("darkblue", "orange", "darkgreen"),
+                              labels = c("Binomial", "Poisson", "Exponential")) +
+            {if (exists("binom")) get("binom")} +
+            {if (exists("pois")) get("pois")} +
+            {if (exists("expon")) get("expon")}
+    })
+})
--- CLT_Visualisation/ui.R
+++ CLT_Visualisation/ui.R
@@ -1,1 +1,58 @@
 library(shiny)
+
+shinyUI(fluidPage(
+    titlePanel("Visualising CLT"),
+    h3("Documentation:"),
+    ("This app is aimed at helping to visualise Central Limit Theorem (CLT). From
+    Wikipedia we can read that 'when independent random variables are added, their
+    properly normalized sum tends toward a normal distribution (informally a bell
+    curve) even if the original variables themselves are not normally distributed'."),
+    br(), br(),
+    ("In the side bar, you can decide from what type of distribution (binomial,
+    Poisson and/or exponential) you would like to sample, how many samples you
+    would like to have, and how many observations you would like in each sample.
+    These are then standardised before being plotted.
+    You will see that as both the number of samples and the number of observations
+    per sample increases, the distribution of the means of these samples tends
+    towards a normal distribution."),
+    br(), br(),
+    ("You can display multiple distributions at a time. The normal distribution
+    density plot is fixed and serves as a reference. It has a mean of 0 and standard
+    deviation of 1."),
+    br(), br(),
+    sidebarLayout(
+        sidebarPanel(
+            h3("Pick and Choose"),
+            h4("Samples from Binomial Distribution"),
+            
+            checkboxInput("check_binom", "Binomial"),
+            conditionalPanel(
+                condition = "input.check_binom == true",
+                sliderInput("slider_binom_sam", "How many samples? The mean of
+                            each of these will be taken and then plotted",
+                            min = 1, max = 10000, value = 5),
+                sliderInput("slider_binom_obs", "How many observations per sample?",
+                            min = 1, max = 100, value = 2)),
+            
+            checkboxInput("check_pois", "Poisson"),
+            conditionalPanel(
+                condition = "input.check_pois == true",
+                sliderInput("slider_pois_sam", "How many samples? The mean of
+                            each of these will be taken and then plotted",
+                            min = 1, max = 10000, value = 5),
+                sliderInput("slider_pois_obs", "How many observations per sample?",
+                            min = 1, max = 100, value = 2)),
+            
+            checkboxInput("check_exp", "Exponential"),
+            conditionalPanel(
+                condition = "input.check_exp == true",
+                sliderInput("slider_exp_sam", "How many samples? The mean of
+                            each of these will be taken and then plotted",
+                            min = 1, max = 10000, value = 5),
+                sliderInput("slider_exp_obs", "How many observations per sample?",
+                            min = 1, max = 100, value = 2)),
+        ),
+        mainPanel(
+            plotOutput("plot")
+        ))
+))
